a12507d863f3b6dbb6f1b9091c11ee6993447876
Sandra Martineau <sandramartineau@yahoo.fr>
2018-06-14 16:27:47 +0200

    if et for

---
+++ basefor.py
@@ -0,0 +1,4 @@
+fruits = ['orange', 'apple', 'pear', 'banana', 'kiwi']
+
+for f in fruits:
+    print(f)
---
+++ baseif.py
@@ -0,0 +1,6 @@
+i = 13
+
+if i == 12:
+    print("i egal a 12")
+else:
+    print("i n est pas egal a 12")
