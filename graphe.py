fb88154e65d88010a2ce22b8135f99787200342b
Sandra Martineau <sandramartineau@yahoo.fr>
2018-06-15 16:05:58 +0200

    graphes final

--- graphe.py
+++ graphe.py
@@ -5,20 +5,30 @@ class Graphe:
     def __init__(self):
         #attribut
         self.noeud = []
-        #self.arete = arete
-        #print('creation noeud ' + self.noeud)
-        #print('creattion arete ' + self.arete)
+        self.arete = []
 
     # declarer une methode
     def ajouterNoeud(self, noeud):
         print(noeud + " est ajoute ")
         self.noeud.append(noeud)
 
-  #  def ajouterArete(self,arete):
-  #      print(self.arete + " est ajoutee ")
+    def ajouterArete(self,noeud1, noeud2):
+        print("Ajouter arete entre " + noeud1 + " et " + noeud2)
+        self.arete.append(noeud1 + ":" + noeud2)
+
+    def existArete(self, n1, n2):
+        for a in self.arete:
+            if a == (n1 + ':' + n2):
+                return True
+        return False
 
 
 graphe = Graphe()
 
-graphe.ajouterNoeud('noeud2')
-#graphe.ajouterArete('arete2')
+graphe.ajouterNoeud('n1')
+graphe.ajouterNoeud('n2')
+graphe.ajouterNoeud('n3')
+graphe.ajouterArete('n1', 'n2')
+
+print(graphe.existArete('n1', 'n2'))
+print(graphe.existArete('n1', 'n3'))
