82cb76159e9d1174aa7d592fdfd0a3c520d9097e
Sandra Martineau <sandramartineau@yahoo.fr>
2018-06-18 12:13:41 +0200

    nb premier 2

--- exo_nb_prem.py
+++ exo_nb_prem.py
@@ -6,15 +6,32 @@
 #
 # print(nbPrem(5))
 
-
 def nbPrem(n):
     for i in range(2,n-1):
         if n % i == 0:
+            # print(str(n) + " divisible par " + str(i))
             return False
     return True
 
-v = nbPrem(5)
-v1 = nbPrem(10)
+# v = nbPrem(5)
+# v1 = nbPrem(10)
+#
+# print(v)
+# print(v1)
 
-print(v)
-print(v1)
+##################
+# def listNb(n):
+#     list = []
+#     for i in range(2,99):
+#         nbPrem(n)
+#         list.append(n)
+#         print(list)
+#         return
+
+
+def listNb():
+    for n in range(2,99):
+        if nbPrem(n) == True:
+            print("Nombre premier " + str(n))
+
+listNb()
